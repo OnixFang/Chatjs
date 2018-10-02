1785e1f1673c957bab55066c57148c7388a22809
Ulises Taveras <twily.dev@outlook.com>
2018-10-02 13:56:45 -0400

    Rename separate http version of authentication service

--- app/scripts/services/authentication-http.js
+++ app/scripts/services/authentication-http.js
@@ -1,24 +1,20 @@
 (() => {
     const app = angular.module('chatjs');
 
-    function authentication($resource, $window, $location) {
-        // API resource path for user data
-        const resource = $resource('/user/:id', { id: '@id' });
-
+    function authentication($http, $window, $location) {
         function getUser(userId) {
-            return resource.get({ id: userId }).$promise
-                .then((response) => {
-                    console.log('GET successful.');
-                    console.log(response);
+            return $http.get('/user/' + userId)
+                .then(() => {
+                    $location.path('/login');
                 }, (error) => {
-                    console.log('Error performing GET.');
-                    console.log(error);
+                    $window.alert('Error registering your user: ' + error.data);
                 });
         }
 
         function saveUser(user) {
-            return resource.save(user).$promise
+            return $http.post('/user/', user)
                 .then(() => {
+                    $window.alert('Register successful');
                     $location.path('/login');
                 }, (error) => {
                     $window.alert('Error registering your user: ' + error.data);
